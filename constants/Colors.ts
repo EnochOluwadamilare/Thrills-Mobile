4039982c95f40e0c299648e105b2c917448be4f7
Oyerinde Enoch <oyerindeenoch@gmail.com>
2024-12-07 23:06:25 +0100

    Correction to Color Mode

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,16 +8,18 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#000000',
+    background: '#ffffff',
+    buttonColor: "linear-gradient(90deg, #5E308A, #A21C7D, #FF006C)",
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#ffffff',
+    background: '#212121',
+    buttonColor: "linear-gradient(90deg, #5E308A, #A21C7D, #FF006C)",
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
